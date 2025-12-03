c273730c6503e0f00a0d9213ff1d953a706d64a9
Dikshaaa1212 <ghimirediksha222@gmail.com>
2025-12-03 22:28:22 +0545

    Task 1 Done

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10, 5);
+
+
+            Console.WriteLine(rect.ShowDetails());
+            Console.WriteLine($"Area: {rect.GetArea()}");
+            Console.WriteLine($"Perimeter: {rect.GetPerimeter()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task1/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+
+        public Rectangle(double length, double breadth) =>
+            (this.length, this.breadth) = (length, breadth);
+
+        public double GetArea() => length * breadth;
+
+        public double GetPerimeter() => 2 * (length + breadth);
+
+        public string ShowDetails() =>
+            $"Length: {length}, Breadth: {breadth}";
+    }
+}
